040236b7634a99a7e1a233e3c373fec2fb595219
evilkissyou <evilkissyou@inbox.lv>
2021-10-02 10:36:13 +0300

    Created Airplane Telegram Bot project with necessary dependencies

---
+++ src/main/java/com/evilkissyou/airplanetelegrambot/AirplaneTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.evilkissyou.airplanetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneTelegramBotApplication.class, args);
+	}
+
+}
