952bc78cd5b742bf5ff36104a8d7e78a682de768
evilkissyou <evilkissyou@inbox.lv>
2021-10-03 12:37:35 +0300

    Created main class for registering and launching the telegram bot. Added initialization of static maxAirplaneNumber variable to init method.

--- src/main/java/com/evilkissyou/airplanetelegrambot/AirplaneTelegramBotApplication.java
+++ src/main/java/com/evilkissyou/airplanetelegrambot/AirplaneTelegramBotApplication.java
@@ -1,13 +1,35 @@
 package com.evilkissyou.airplanetelegrambot;
 
+import com.evilkissyou.airplanetelegrambot.controller.MessageDispatcher;
+import com.evilkissyou.airplanetelegrambot.data.AirplaneRepository;
+import com.evilkissyou.airplanetelegrambot.telegram.Bot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class AirplaneTelegramBotApplication {
 
+	@Autowired
+	AirplaneRepository airplaneRepository;
+
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
+		TelegramBotsApi botsApi = new TelegramBotsApi();
+		try {
+			botsApi.registerBot(new Bot());
+		} catch (TelegramApiException ignored) {
+		}
 		SpringApplication.run(AirplaneTelegramBotApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		MessageDispatcher.maxAirplaneNumber = airplaneRepository.findAll().size();
+	}
 }
