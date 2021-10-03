0a299f28ade94ad111ef798036d533ffc7d2ec75
evilkissyou <evilkissyou@inbox.lv>
2021-10-03 12:40:51 +0300

    Added private modifiers to fields. Added join table through list of users, which already seen a particular airplane in the bot. Added equals and hashcode methods so that MessageDispatcher correctly identifies unique airplanes.

--- src/main/java/com/evilkissyou/airplanetelegrambot/data/Airplane.java
+++ src/main/java/com/evilkissyou/airplanetelegrambot/data/Airplane.java
@@ -1,10 +1,8 @@
 package com.evilkissyou.airplanetelegrambot.data;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.experimental.FieldDefaults;
-
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "airplanes")
@@ -12,19 +10,27 @@ public class Airplane {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer airplaneId;
-    String name;
-    String designator;
-    String wakeTurbulence;
-    String configuration;
-    String cruiseLevel;
-    String trueAirSpeed;
-    String rateOfClimb;
-    String rateOfDescent;
-    String category;
-    String dimensions;
-    String notes;
-    String imageUrl;
+    private Integer airplaneId;
+    private String name;
+    private String designator;
+    private String wakeTurbulence;
+    private String configuration;
+    private String cruiseLevel;
+    private String trueAirSpeed;
+    private String rateOfClimb;
+    private String rateOfDescent;
+    private String category;
+    private String dimensions;
+    private String notes;
+    private String imageUrl;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_airplanes",
+            joinColumns = @JoinColumn(name = "airplane_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> users;
 
     public Airplane() {
     }
@@ -48,6 +54,14 @@ public class Airplane {
         return name;
     }
 
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
     @Override
     public String toString() {
         return "<strong>" + name + "</strong>" + "\n" +
@@ -62,5 +76,19 @@ public class Airplane {
                 "Dimensions = <i>" + dimensions + "</i>" + "\n" +
                 "Notes = <i>" + notes + "</i>" + "\n" +
                 "<a href=\"" + imageUrl + "\">Image</a>";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airplane airplane = (Airplane) o;
+        return Objects.equals(airplaneId, airplane.airplaneId) &&
+                Objects.equals(name, airplane.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(airplaneId, name);
     }
 }
