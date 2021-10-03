057eb8c1e42e8077eabadff74aca4d07a470aab9
evilkissyou <evilkissyou@inbox.lv>
2021-10-03 12:59:39 +0300

    Created configuration file for the bot, which is using BOT_TOKEN and BOT_NAME environmental variables. Also, contains constructor for MessageDispatcher.

---
+++ src/main/java/com/evilkissyou/airplanetelegrambot/telegram/BotConfig.java
@@ -0,0 +1,40 @@
+package com.evilkissyou.airplanetelegrambot.telegram;
+
+import com.evilkissyou.airplanetelegrambot.controller.MessageDispatcher;
+import com.evilkissyou.airplanetelegrambot.data.AirplaneRepository;
+import com.evilkissyou.airplanetelegrambot.data.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BotConfig {
+
+    @Value("#{environment.BOT_TOKEN}")
+    private String botToken;
+
+    @Value("#{environment.BOT_NAME}")
+    private String botName;
+
+    @Autowired
+    AirplaneRepository airplaneRepository;
+
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Bean
+    public Bot bot() {
+        Bot bot = new Bot();
+        bot.setBotUsername(botName);
+        bot.setBotToken(botToken);
+        return bot;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public MessageDispatcher messageDispatcher() {
+        return new MessageDispatcher(airplaneRepository, usersRepository);
+    }
+}
