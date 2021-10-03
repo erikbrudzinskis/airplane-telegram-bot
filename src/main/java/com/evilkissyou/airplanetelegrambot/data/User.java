ba2fb61b0f6efe6723a9530af89a2b33be8816d0
evilkissyou <evilkissyou@inbox.lv>
2021-10-03 12:42:58 +0300

    Created user entity which is related to users table. Contains serial user_id and chat_id assigned by telegram. Also contains list of airplanes, which a particular user already seen in the app.

---
+++ src/main/java/com/evilkissyou/airplanetelegrambot/data/User.java
@@ -0,0 +1,43 @@
+package com.evilkissyou.airplanetelegrambot.data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    private Integer chatId;
+
+    @ManyToMany (fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_airplanes",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "airplane_id")
+    )
+    private List<Airplane> airplanes;
+
+    public User() {
+    }
+
+    public User(Integer chatId) {
+        this.chatId = chatId;
+    }
+
+    public Integer getChatId() {
+        return chatId;
+    }
+
+    public List<Airplane> getAirplanes() {
+        return airplanes;
+    }
+
+    public void setAirplanes(List<Airplane> airplanes) {
+        this.airplanes = airplanes;
+    }
+}
