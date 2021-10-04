7b9c1c7c6c0d94f4f8fc0d6d3396aba5684dd44d
evilkissyou <evilkissyou@inbox.lv>
2021-10-04 16:11:27 +0300

    Created constants for ease of access

---
+++ src/main/java/com/evilkissyou/airplanetelegrambot/constants/Constants.java
@@ -0,0 +1,16 @@
+package com.evilkissyou.airplanetelegrambot.constants;
+
+public final class Constants {
+    public static final String NEXT = "Next";
+    public static final String PROGRESS = "Progress";
+    public static final String RESET = "Reset";
+    public static final String HELP = "Help";
+    public static final String COMMAND_NEXT = "/next";
+    public static final String COMMAND_PROGRESS = "/statistics";
+    public static final String COMMAND_RESET = "/reset";
+    public static final String COMMAND_HELP = "/help";
+    public static final String GET_AIRPLANE_INFO = "Get airplane info";
+    public static final String DESCRIPTION = "This bot checks your knowledge about certain aircraft. " +
+            "First, it gives you the name of an aircraft, so that you can try to remember the aircraft characteristics and looks. " +
+            "Then, it shows you the answers with a pic.";
+}
